0fe78be2be27a5ea8beaaa1ca6a615d9651ddca7
Your Name <you@example.com>
2024-08-30 06:57:29 +0800

    Site updated: 2024-08-30 06:57:29

---
+++ js/wrapImage.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    wrapImageWithFancyBox();
+});
+
+setInterval(function () {
+    wrapImageWithFancyBox();
+}, 2000);
+
+
+/**
+ * Wrap images with fancybox support.
+ */
+
+function wrapImageWithFancyBox() {
+    $('img').not('[data-fancybox="images"] img').not('.sidebar-image img').not('#author-avatar img').not(".mdl-menuimg").not(".something-else-logo img").not('.avatar').not('.share-body img').each(function() {
+        var $image = $(this);
+        var alt = $image.attr('alt');
+        var src = $image.attr('src');
+        $imageWrapLink = $image.wrap('<a data-fancybox=images data-caption="'+ alt +'" href="' + src + '"></a>');
+    });
+
+    $('.qrcode').attr('data-fancybox', 'images');
+
+    $().fancybox({
+        selector: '[data-fancybox="images"]',
+        thumbs: false,
+        hash: true,
+        loop: false,
+        fullScreen: false,
+        overlayOpacity: 0,
+        slideShow: false,
+        protect: true,
+    });
+}
