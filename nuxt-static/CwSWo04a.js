488e35c81954782140eea732f6c6c73c86e7e53b
Your Name <you@example.com>
2025-05-29 07:41:58 +0800

    Power by manesec

---
+++ nuxt-static/CwSWo04a.js
@@ -0,0 +1,1 @@
+import{_ as o,c as r,o as t,Y as n}from"./DiA_zzlC.js";const s={};function c(e,a){return t(),r("strong",null,[n(e.$slots,"default")])}const _=o(s,[["render",c]]);export{_ as default};
